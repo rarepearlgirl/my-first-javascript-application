71f4bee11e63646b0d0263b6d93f59f1f3b2ce4f
Eva Nikolaieva <draculahgirl@gmail.com>
2022-12-06 16:11:08 +0100

    update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 alert('Hello world')
+let favoriteFood = 'meat';
+document.write(favoriteFood);
+
+let favoriteFood2 = '+tomatoes';
+document.write(favoriteFood2);
+
+let favoriteFood3 = '+honey';
+document.write(favoriteFood3);
